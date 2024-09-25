e1eec2d75417b49267273e008e8c065b8cba58ab
usersilva <silvianelson2003@gmail.com>
2024-09-25 20:47:53 +0300

    the 2nd day of my pain

---
+++ Controllers/CompanyController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace regtest.Controllers
+{
+  public class CompanyController : Controller
+  {
+    private regtest.CompanyController db = new regtest.CompanyController();
+
+    // Получить список всех компаний
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    // Создать новую компанию
+    [HttpPost]
+    public ActionResult Create(Company company)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Company.Add(company);
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(company);
+    }
+
+    // Обновить существующую компанию
+    [HttpPost]
+    public ActionResult Edit(Company company)
+    {
+      if (ModelState.IsValid)
+      {
+        db.Entry(company).State = EntityState.Modified;
+        db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+      return View(company);
+    }
+
+    // Удалить компанию
+    [HttpPost]
+    public ActionResult Delete(int id)
+    {
+      var company = db.Company.Find(id);
+      if (company != null)
+      {
+        db.Company.Remove(company);
+        db.SaveChanges();
+      }
+      return RedirectToAction("Index");
+    }
+  }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется следующим образом
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("regtest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("regtest")]
+[assembly: AssemblyCopyright("© , 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("349078b5-6635-4ebc-81f9-7aa15219fcf3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      Дополнительный номер версии
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
